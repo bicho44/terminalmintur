61668873492cbc9d3bfc2ac1809c0bf8cedae6be
Federico <admin@imgdigital.com.ar>
2017-07-24 00:07:50 -0300

    jQuery tercer intento

--- imgd/imgd_funciones.php
+++ imgd/imgd_funciones.php
@@ -16,11 +16,11 @@ function load_external_jQuery() {
 	wp_deregister_script( 'jquery' );
 
 // Check to make sure Google's library is available
-	$link = 'ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
-	$try_url = @fopen("http://".$link,'r');
+	$link = '//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+	$try_url = @fopen("http:".$link,'r');
 	if( $try_url !== false ) {
 		// If it's available, get it registered
-		wp_register_script('jquery',$try_url, false, null, true);
+		wp_register_script('jquery',$link, false, null, true);
 	} else {
 		// Register the local file if CDN fails
 		wp_register_script('jquery', get_template_directory_uri().'/assets/js/vendor/jquery.min.js', __FILE__, false, '2.2.4', true);
